4e752d77620aec995945cab6abda3368a57bf176
priyanshu-k712 <pkamani712@gmail.com>
2025-01-09 04:48:32 +0530

    Learning Data Types in JS

---
+++ 01_basics/02_Data_Types.js
@@ -0,0 +1,11 @@
+"use strict" //treat all code AS newer version of js we dont need to write it
+//alert(3+3); //We are using nodejs not a browser
+let name = "Priyanshu" //String
+let age = 20 //Number rnage: 2^53 if it exceeds the range it uses Bigint
+let isLoggedIn = false //Boolean
+//null is also a datatype. It is a standalone Value. It is a representation of empty value. typeof gives object
+//undefined: If value is not given. typeof gives undefined
+//symbol : unique.
+//object
+
+console.log(typeof(false));
