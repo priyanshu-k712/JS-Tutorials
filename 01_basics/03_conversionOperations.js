3273dbe8144efabb8098047d54a352537b98495a
priyanshu-k712 <pkamani712@gmail.com>
2025-01-09 05:07:37 +0530

    Learning Datatype Confusion and Type Conversions

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = 23;
+console.log(typeof score);
+// let Score = "23";
+// let Score = "23abc";
+// let Score = null;
+let Score = undefined;
+console.log(typeof Score);
+let valueInNumber = Number(Score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc"=>NaN
+// true=>1, flase=>0
+
+// let isloggedIn = 1; true
+// let isloggedIn = ""; false
+let isloggedIn = "Harry"; //true
+let booleanValue = Boolean(isloggedIn);
+console.log(typeof booleanValue);
+console.log(booleanValue);
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
