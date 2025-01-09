d871f57198983ad6fcf46db986d76acc774dba48
priyanshu-k712 <pkamani712@gmail.com>
2025-01-09 14:29:22 +0530

    Deep study of operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,27 +1,50 @@
-let score = 23;
-console.log(typeof score);
-// let Score = "23";
-// let Score = "23abc";
-// let Score = null;
-let Score = undefined;
-console.log(typeof Score);
-let valueInNumber = Number(Score);
-console.log(valueInNumber);
-console.log(typeof valueInNumber);
+// let score = 23;
+// console.log(typeof score);
+// // let Score = "23";
+// // let Score = "23abc";
+// // let Score = null;
+// let Score = undefined;
+// console.log(typeof Score);
+// let valueInNumber = Number(Score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
 
-// "33" => 33
-// "33abc"=>NaN
-// true=>1, flase=>0
+// // "33" => 33
+// // "33abc"=>NaN
+// // true=>1, flase=>0
 
-// let isloggedIn = 1; true
-// let isloggedIn = ""; false
-let isloggedIn = "Harry"; //true
-let booleanValue = Boolean(isloggedIn);
-console.log(typeof booleanValue);
-console.log(booleanValue);
+// // let isloggedIn = 1; true
+// // let isloggedIn = ""; false
+// let isloggedIn = "Harry"; //true
+// let booleanValue = Boolean(isloggedIn);
+// console.log(typeof booleanValue);
+// console.log(booleanValue);
 
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+let value = 33;
+let negativeValue = -value;
+console.log(typeof negativeValue);
+// console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+
+// let str1 = "Hello";
+// let str2 = " Harry";
+// let str3 = str1+str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2"); //Topremitive of documentation shows guidlines are given
+
+// console.log(+true);
+// console.log(+"");
