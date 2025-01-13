545cfaff8755b54e7cd5bd5012129e2843123b1f
priyanshu-k712 <pkamani712@gmail.com>
2025-01-13 12:15:22 +0530

    Learning about comparisions between different Data Types.

---
+++ 01_basics/04_Comparision.js
@@ -0,0 +1,15 @@
+// console.log(2 > 1);
+// console.log("2" >= 1);
+// console.log(2!=1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); //Null is converted into zero and then compared.
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// Strict check is nothing but checking the data type and condition togrther which is ===.
+
+console.log('2' === 2); //"2" is string and 2 is number hence answer will be false
