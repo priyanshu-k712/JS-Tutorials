80e9f58ff805eba10afeae7b01b2f6f6784eb555
priyanshu-k712 <pkamani712@gmail.com>
2025-01-15 13:21:39 +0530

    Learning Math and about Number Datatype

---
+++ 01_basics/07_Numbers_Math.js
@@ -0,0 +1,41 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(300);
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// // const otherNumber = 23.3456;
+// const otherNumber = 123.4567;
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_VALUE);
+
+// ---------------- Maths-------------------- //
+
+// console.log(Math); //Write this in inspect element console in browser to learn about all possible attributes
+
+// console.log(Math.abs(4));
+
+// console.log(Math.round(4.33));
+// console.log(Math.round(4.63));
+// console.log(Math.floor(4.63));
+// console.log(Math.ceil(4.22));
+
+// console.log(Math.min(4, 5, 6, 1));
+// console.log(Math.max(4, 5, 6, 1));
+
+// console.log(Math.random()); //Value is always between 0-1
+// console.log(Math.floor(Math.random() * 10) + 1); //Value will not be 0 and >0 always.
+
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // Basic syntax to generate a random number between min and max range.
+
+
